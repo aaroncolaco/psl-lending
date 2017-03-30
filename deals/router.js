68f7e6e24581222de822ba28414d6a6f7c1ff2f1
Aaron Colaco <aaron_colaco@persistent.com>
2017-03-30 09:34:47 +0530

    CORS

--- deals/router.js
+++ deals/router.js
@@ -5,6 +5,17 @@ const router = express.Router();
 
 const controller = require('./controller');
 
+// middleware to allow CORS
+router.use(function(req, res, next) {
+  var origin = req.headers.origin;
+
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.get('/', controller.getAllDeals);
 router.get('/:id', controller.getDealById);
 router.post('/', controller.createDeal);
--- users/router.js
+++ users/router.js
@@ -5,6 +5,17 @@ const router = express.Router();
 
 const controller = require('./controller');
 
+// middleware to allow CORS
+router.use(function(req, res, next) {
+  var origin = req.headers.origin;
+
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.get('/:id', controller.getUserById);
 router.post('/', controller.createUser);
 router.post('/:id', controller.updateUser);
