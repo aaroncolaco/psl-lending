ecb965346e2a63de6b4ecac5d3780e88db952bc0
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-04-27 19:34:15 +0530

    Print error msg properly for FCM

--- notifications/events.js
+++ notifications/events.js
@@ -61,7 +61,7 @@ const notifyUser = (eventName, message, userEthAccount) => {
         return logError(eventName, Error("Cannot send notification to unknown Ethereum user account: " + userEthAccount));
       }
       lib.notify(user.ethAccount, message)
-        .then(response => console.log("Successfully sent message:", response));
+        .then(response => console.log("Then Block: ", JSON.stringify(response)));
     })
     .catch(err => logError(eventName, err));
 };
